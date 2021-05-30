af692038b511b0c824cd36b7c57773819f298242
Lakshman.S <haranhari_38@outlook.com>
2021-05-30 20:21:44 +0530

    Updated Service & Interface for Todo List

--- src/app/todo-list/ITodo.interface.ts
+++ src/app/todo-list/ITodo.interface.ts
@@ -1,13 +1,17 @@
+import { v4 as uuidv4 } from 'uuid';
+
 export interface ITodo {
     Task: string;
     IsCompleted: boolean;
 }
 
 export class Todo implements ITodo {
+    Id: string;
     Task: string;
     IsCompleted: boolean;
 
     constructor(Task: string, IsCompleted: boolean) {
+        this.Id = uuidv4();
         this.Task = Task;
         this.IsCompleted = IsCompleted;
     }
