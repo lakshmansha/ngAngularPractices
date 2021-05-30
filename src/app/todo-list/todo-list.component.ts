3cd1fcea3aeb26b3fb22fb2a89bf6e03d84d791d
Lakshman.S <haranhari_38@outlook.com>
2021-05-30 20:22:22 +0530

    Option to Add Todo List with Reactive Forms.

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+
 import { Todo } from './ITodo.interface';
 import { TodoListService } from './todo-list.service';
 
@@ -9,10 +11,19 @@ import { TodoListService } from './todo-list.service';
   styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent implements OnInit {
+  //#region Entry Variables
+
+  view: string;
+  entryForm: FormGroup;
+  todoId: string;
+
+  //#endregion
 
   TodoList: Todo[] = [];
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private formBuilder: FormBuilder, private service: TodoListService) {
+    this.createForm();
+  }
 
   ngOnInit(): void {
     this.PageLoad();
@@ -21,5 +32,50 @@ export class TodoListComponent implements OnInit {
   PageLoad() {
     this.TodoList = this.route.snapshot.data.responses['Todos'];
   }
+
+  //#region Add Todo Variables
+
+  createForm() {
+    this.view = "Add";
+    this.entryForm = this.formBuilder.group({
+      Task: ['', Validators.required],
+      IsCompleted: [false],
+    });
+  }
+
+  resetForm() {
+    this.todoId = "";
+    this.view = "Add";
+    this.entryForm.reset();
+  }
+
+  updateForm(todo: Todo) {
+    this.todoId = todo.Id;
+    this.view = "Update";
+    this.entryForm.setValue({
+      Task: todo.Task,
+      IsCompleted: todo.IsCompleted
+    });
+  }
+
+  Submit() {
+    const todo$ =
+      this.todoId !== ''
+        ? this.service.updateTodo(this.todoId, this.entryForm.value)
+        : this.service.addTodo(this.entryForm.value);
+    todo$
+      .subscribe(
+        (todos) => {
+          this.TodoList = todos;
+          alert(`${this.view} Successfully`);
+          this.resetForm();
+        },
+        (error) => {
+          console.debug(`Todo Entry error: ${error}`);
+        }
+      );
+  }
+
+  //#endregion
 
 }
